5c3b03a020d8c5436023a9c89180f19cad2f9282
Brad Henderson <elliotbradly@gmail.com>
2024-03-21 17:58:23 -0400

    uses window

--- 122.blender/000.quest.blender.js
+++ 122.blender/000.quest.blender.js
@@ -1,4 +1,4 @@
-global.BLENDER = require("../dist/122.blender/hunt");
-global.BLENDER.ActBld = require("../dist/122.blender/00.blender.unit/blender.action");
+window.BLENDER = require("../dist/122.blender/hunt");
+window.BLENDER.ActBld = require("../dist/122.blender/00.blender.unit/blender.action");
 
-global.BLENDER.MQTT = require("async-mqtt");
+window.BLENDER.MQTT = require("async-mqtt");
--- public/jsx/122.blender.js
+++ public/jsx/122.blender.js
@@ -1,10 +1,8 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-(function (global){(function (){
-global.BLENDER = require("../dist/122.blender/hunt");
-global.BLENDER.ActBld = require("../dist/122.blender/00.blender.unit/blender.action");
+window.BLENDER = require("../dist/122.blender/hunt");
+window.BLENDER.ActBld = require("../dist/122.blender/00.blender.unit/blender.action");
 
-global.BLENDER.MQTT = require("async-mqtt");
-}).call(this)}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+window.BLENDER.MQTT = require("async-mqtt");
 },{"../dist/122.blender/00.blender.unit/blender.action":2,"../dist/122.blender/hunt":36,"async-mqtt":39}],2:[function(require,module,exports){
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
